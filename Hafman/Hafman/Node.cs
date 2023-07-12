ffdca56d013115be37f449aae9caf42d3d5c087c
Avagadra <113332857+Avagadra@users.noreply.github.com>
2023-07-12 17:15:22 +0700

    Add files via upload

---
+++ Hafman/Hafman/Node.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hafman
+{
+    public class Node //класс узлов
+    {
+        public char Symbol { get; set; } //символы
+        public int Frequency { get; set; } //частота
+        public Node Right { get; set; } //правый узел
+        public Node Left { get; set; } //левый узел
+
+        public List<bool> Traverse(char symbol, List<bool> data)
+        {
+            // Лист
+            if (Right == null && Left == null)
+            {
+                if (symbol.Equals(this.Symbol))
+                {
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                List<bool> left = null;
+                List<bool> right = null;
+
+                if (Left != null)
+                {
+                    List<bool> leftPath = new List<bool>();
+                    leftPath.AddRange(data);
+                    leftPath.Add(false);
+
+                    left = Left.Traverse(symbol, leftPath);
+                }
+
+                if (Right != null)
+                {
+                    List<bool> rightPath = new List<bool>();
+                    rightPath.AddRange(data);
+                    rightPath.Add(true);
+                    right = Right.Traverse(symbol, rightPath);
+                }
+
+                if (left != null)
+                {
+                    return left;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+}
---
+++ Hafman/Hafman/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Hafman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the string:");
+            string input = Console.ReadLine(); // вводим строку которую будем сжимать
+            Hafman_tree haffmanTree = new Hafman_tree();
+
+            // Строим дерево
+            haffmanTree.Build(input);
+
+            // Шифруем
+            BitArray encoded = haffmanTree.Encode(input);
+
+            Console.Write("Encoded: "); // выводим зашифрованый вариант
+            foreach (bool bit in encoded)
+            {
+                Console.Write((bit ? 1 : 0) + "");
+            }
+            Console.WriteLine();
+
+            // Расшифровываем
+            string decoded = haffmanTree.Decode(encoded);
+
+            Console.WriteLine("Decoded: " + decoded); // выводим расшифровку
+
+            Console.ReadLine();
+        }
+    }
+}
